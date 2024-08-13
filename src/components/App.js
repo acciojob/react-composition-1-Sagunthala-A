ff9bb567894a09692c13a224767a9cb19e703c59
sugu-mar <sugumargpt@gmail.com>
2024-08-14 00:23:16 +0530

    edited

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,15 @@ const App = () => {
   const arr =[
     {
       title:"Tab 1",
-      content: `This is content for Tab 1`
+      content: "This is content for Tab 1"
     },
     {
       title:"Tab 2",
-      content: `This is content for Tab 2`
+      content: "This is content for Tab 2"
     },
     {
       title:"Tab 3",
-      content: `This is content for Tab 3`
+      content: "This is content for Tab 3"
     }
   ]
   return (
